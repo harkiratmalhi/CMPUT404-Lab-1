9f858cda1a09680c2f60f0a29908b69e2b612c89
Harkirat Malhi <malhi@Harkirats-MacBook-Pro.local>
2022-09-12 19:17:31 -0600

    2nd commit

--- question2_lab1.py
+++ question2_lab1.py
@@ -1,4 +1,6 @@
 import requests
 
 print(requests.__version__)
-r = requests.get('https://www.google.com/
+r = requests.get('https://www.google.com/')
+r1 = requests.get('https://raw.githubusercontent.com/harkiratmalhi/CMPUT404-Lab-1/master/question2_lab1.py').content
+print(r1)
